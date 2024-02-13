8674cc697eae100b57a4de53db477f503b883b16
Brian Kipkoech <kipkoechbrian0@gmail.com>
2024-02-13 16:39:13 +0300

    configur spring security to show custom login page

---
+++ src/main/java/com/example/mvssecurity/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.mvssecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails suzan = User.builder()
+                .username("suzan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, suzan);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer.anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form.loginPage("/showLogin")
+                                .loginProcessingUrl("/authenticate")
+                                .permitAll()
+                        );
+        return http.build();
+    }
+}
