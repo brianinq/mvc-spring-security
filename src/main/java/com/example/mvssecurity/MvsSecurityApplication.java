187d4a0e4696729807204f3f5c86e111ff53e7ac
Brian Kipkoech <kipkoechbrian0@gmail.com>
2024-02-13 15:43:03 +0300

    initial commit

---
+++ src/main/java/com/example/mvssecurity/MvsSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvsSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvsSecurityApplication.class, args);
+    }
+
+}
